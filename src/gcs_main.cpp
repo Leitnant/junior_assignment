a613658276a32cd6b8caf7ca395d1d33c93d836c
Jaagup Jagomägi <jaagup.jagomagi@gmail.com>
2025-06-02 19:35:37 +0300

    asendasin if-id case switchiga.

--- src/gcs_main.cpp
+++ src/gcs_main.cpp
@@ -20,32 +20,41 @@ int main() {
     float x, y, alt;
     while (1) {
         if (gcs.isConnected()){
-            printf("\n[GCS]: ACTION       [n]\n");
-            printf("[GCS]: Arm          [1]\n");
-            printf("[GCS]: Disarm       [2]\n");
-            printf("[GCS]: Goto         [3]\n");
-            printf("[GCS]: Choose acton: ");
-            cin >> choice;
-            if (choice == ARM){
-                printf("[GCS]: Arming drone...\n");
-                gcs.send_arm();
-            } else if (choice == DISARM) {
-                printf("[GCS]: Disarming drone...\n");
-                gcs.send_disarm();
-            } else if (choice == GOTO){
-                printf("[GCS]: Enter X:   ");
-                cin >> x;
-                printf("[GCS]: Enter Y:   ");
-                cin >> y;
-                printf("[GCS]: ENTER ALT: ");
-                cin >> alt;
-                printf("[GCS]: Sending target...\n");
-                gcs.send_goto(x, y, alt);
-            } else {
-                printf("[GCS]: Invalid input!\n");
-            }
-        }
-    }
+            printf("\n[GCS]: ACTION        [n]\n");
+            printf(  "[GCS]: Arm           [1]\n");
+            printf(  "[GCS]: Disarm        [2]\n");
+            printf(  "[GCS]: Goto          [3]\n");
+            printf(  "[GCS]: Choose action: ");
 
+            cin >> choice;
+
+            switch (choice){
+                case ARM:
+                    printf("[GCS]: Arming drone...\n");
+                    gcs.send_arm();
+                    break;
+                case DISARM:
+                    printf("[GCS]: Disarming drone...\n");
+                    gcs.send_disarm();
+                    break;
+                case GOTO:
+                    printf("[GCS]: Enter X:   ");
+                    cin >> x;
+                    printf("[GCS]: Enter Y:   ");
+                    cin >> y;
+                    printf("[GCS]: ENTER ALT: ");
+                    cin >> alt;
+                    printf("[GCS]: Sending target...\n");
+                    gcs.send_goto(x, y, alt);
+                    break;
+                default:
+                    printf("[GCS]: Invalid input!\n");
+                    break;
+
+            }
+
+        }
+
+    }
     return 0;
 }

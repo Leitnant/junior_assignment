6a1fc69724c3b62d431d88f6632b55dc7a63b9a5
Jaagup Jagomägi <jaagup.jagomagi@gmail.com>
2025-06-11 19:26:22 +0300

    Fix: drone_main.cpp main loop ei kasutanud mitteblokeeruvat loopi andmete saatmiseks.

--- src/drone_main.cpp
+++ src/drone_main.cpp
@@ -9,15 +9,22 @@ int main() {
     lnl::net_address address("127.0.0.1", 4499);
     Drone drone(0.5, 10, address);
 
+    chrono::steady_clock::time_point prevTime = chrono::steady_clock::now();
+
     while (1) {
         if (drone.isConnected()) {
             if (drone.isArmed()) {
                 drone.updatePosition();
             }
-            drone.send_heartbeat();
-            drone.send_position();
-            this_thread::sleep_for(chrono::milliseconds(100));
+            chrono::steady_clock::time_point currentTime = chrono::steady_clock::now();
+
+            if (currentTime - prevTime >= chrono::milliseconds(100)){
+                drone.send_heartbeat();
+                drone.send_position();
+                prevTime = currentTime;
+            }
         }
+        this_thread::sleep_for(chrono::milliseconds(5));
     }
 
     return 0;
